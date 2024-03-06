dc4c168d95b0f30ae3b493cb7c91ddde33f8a211
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-03-06 17:00:55 +0100

    radix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -44,9 +44,14 @@ void counting_sort_radix(int *array, size_t size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-	int max = array[0];
+	int max;
 	size_t i;
 	int exp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	max = array[0];
 
 	for (i = 1; i < size; i++)
 	{
